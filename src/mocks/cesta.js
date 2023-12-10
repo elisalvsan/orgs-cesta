b4e63afb5a30232e5b4dd98f86e9781f9e601de6
Elisa Andrade <elisalvsan@gmail.com>
2023-12-10 01:52:09 -0300

    mocks: adicionando os itens a cesta.js

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,5 +1,11 @@
 import logo from "../../assets/logo.png";
 
+import tomate from "../../assets/frutas/tomate.png";
+import brocolis from "../../assets/frutas/Brócolis.png";
+import batata from "../../assets/frutas/batata.png";
+import abobora from "../../assets/frutas/Abóbora.png";
+import pepino from "../../assets/frutas/Pepino.png";
+
 const cesta = {
   topo: {
     titulo: "Detalhe da cesta",
@@ -11,6 +17,31 @@ const cesta = {
     descricao:
       "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha.",
     preco: "R$ 40,00",
+  },
+  itens: {
+    titulo: "Itens da cesta",
+    lista: [
+      {
+        nome: "Tomate",
+        imagem: tomate,
+      },
+      {
+        nome: "Brócolis",
+        imagem: brocolis,
+      },
+      {
+        nome: "Batata",
+        imagem: batata,
+      },
+      {
+        nome: "Abóbora",
+        imagem: abobora,
+      },
+      {
+        nome: "Pepino",
+        imagem: pepino,
+      },
+    ],
   },
 };
 
